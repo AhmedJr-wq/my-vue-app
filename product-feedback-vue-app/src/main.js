5c68885d8396447fbc9a93cef3e47d6c50e36783
AhmedJr-wq <abulfail.rufai@amalitech.com>
2023-10-31 14:59:39 +0000

    feat: created store for global state management

--- product-feedback-vue-app/src/main.js
+++ product-feedback-vue-app/src/main.js
@@ -5,10 +5,11 @@ import BaseButton from "./components/UI/BaseButton.vue";
 import Alert from "./components/UI/Alert.vue";
 import NotificationAlert from "./components/UI/NotificationAlert.vue";
 import Feedback from "./components/feedbacks/Feedback.vue";
-import {createRouter, createWebHistory} from "vue-router";
 import MainComponent from "./components/MainComponent.vue";
 import GoBackButton from "./components/UI/GoBackButton.vue";
 import Suggestions from "./components/feedbacks/Suggestions.vue";
+import {createRouter, createWebHistory} from "vue-router";
+import { createStore } from "vuex";
 
 const router = createRouter({
     history: createWebHistory(),
@@ -23,9 +24,18 @@ const router = createRouter({
     ]
 })
 
+const store = createStore({
+    state() {
+        return {
+
+        }
+    },
+})
+
 const app = createApp(App)
 
 app.use(router)
+app.use(store)
 
 app.component('base-button', BaseButton)
 app.component('notification-alert', NotificationAlert)
